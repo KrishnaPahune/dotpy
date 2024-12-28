099641cef4bb40108de6acef6109c2f95a5e8af8
Krishna  Pahune <38493878-krishnapahune4@users.noreply.replit.com>
2024-12-28 15:59:35 +0000

    Automatic commit before Assistant edits

--- Resume.py
+++ Resume.py
@@ -1,4 +1,3 @@
-print("Welcome to the Resume generator".center(100,"|"))
 name=input("Name = ")
 age=int(input("Age = "))
 address=input("Address = ")
@@ -8,7 +7,10 @@ degree_college,university,degree_name,cgpa=input("Entr your college name,univers
 hsc_name,hsc_percentage,hsc_group=input("Enter your college name,hsc percentage and group = ").split()
 ssc_name,ssc_percentage=input("Enter your school name and ssc percentage = ").split()
 summary = input("Enter your summary about yourself = ")
-skills = input("Enter your skills = ").split(sep="\n")
+skills = input("Enter your skills = ").split()
+hobbies = input("Enter your hobbies = ").split()
+
+print(" Resume ").center(100,"|")
 
 
 
