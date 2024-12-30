36fd039a6c2821fea4a8e87d0890ee81959d29db
Krishna <krishnapahune4@gmail.com>
2024-12-30 19:23:36 +0530

    useless

--- loops.py
+++ loops.py
@@ -1,30 +1,30 @@
-# name= "Krishna"
-# for i in name:
-#   print(i)
-#   if(i=="h"):
-#     break
+name= "Krishna"
+for i in name:
+  print(i)
+  if(i=="h"):
+    break
 
-# colors = ["Red","Green","Blue","Yellow"]
-# for color in colors:
-#   print(color)
-#   for i in color:
-#     print(i)
+colors = ["Red","Green","Blue","Yellow"]
+for color in colors:
+  print(color)
+  for i in color:
+    print(i)
 
 
-# k = int(input("Enter a number: "))
-# while(k != 10):
-#   print("Try again")
-#   k = int(input("Enter a number: "))
+k = int(input("Enter a number: "))
+while(k != 10):
+  print("Try again")
+  k = int(input("Enter a number: "))
 
-# print("You have entered the correct number")
+print("You have entered the correct number")
 
-# for i in range(12):
-#   if(i==5):
-#     print("Skip the iteration")
-#     continue
-#   print(5,"x",i,"=",5*i)
-#   if(i==10):
-#     break
+for i in range(12):
+  if(i==5):
+    print("Skip the iteration")
+    continue
+  print(5,"x",i,"=",5*i)
+  if(i==10):
+    break
 
 i=0
 while True:
--- range.py
+++ range.py
@@ -9,3 +9,4 @@ it will print the numbers from start to stop-1 with the step as the difference b
 
 for k in range(20,1,-2):
   print(k)
+
