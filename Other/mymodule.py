9d679f2bad89084419faaf2395276a9068bffbb5
Krishna <krishnapahune4@gmail.com>
2025-01-08 18:28:54 +0530

    Jupyter

---
+++ Other/mymodule.py
@@ -0,0 +1,8 @@
+def welcome():
+    print("Welcome to Rashtriya Rifles Buddy! Keep the josh high.")
+
+krishna="Lt.Krishna Gajanan Pahune"
+print(__name__)
+if __name__=="__main__":
+    print(krishna)
+    welcome()
---
+++ Other/mymodule1.py
@@ -0,0 +1,4 @@
+import mymodule as my
+print("Importing from mymodule")
+my.welcome()
+print(my.krishna)
